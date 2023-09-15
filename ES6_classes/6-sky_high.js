f915de7b7c4dc9bfb121ba1e04326f05c4af910e
chaher13 <chahercrbs@gmail.com>
2023-09-15 23:11:23 +0200

    Add: All the tasks of project ES6 CLASSES

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    if (typeof floors === "number") {
+      this._floors = floors;
+    }
+    throw new TypeError("floors must be a number");
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === "string") {
+      this._name = name;
+    }
+    throw new TypeError("name must be a string");
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== "string") {
+      this._code = code;
+    }
+    throw new TypeError("code must be a string");
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
