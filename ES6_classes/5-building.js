acb95a33fec34a7a5c1da485149d6f0b3cce4a34
chaher13 <chahercrbs@gmail.com>
2023-09-15 23:59:03 +0200

    fix: condition of constructor of building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,30 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building &&
+    if (
+      this.constructor !== Building &&
       typeof this.evacuationWarningMessage !== 'function'
     ) {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage'
       );
-    } else {
-      this._sqft = sqft;
-
     }
-
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-
   }
-
-  set sqft(sqft) {
-    if (typeof sqft === 'number') {
-      this._sqft = sqft;
-
-    } else {
-      throw new TypeError('Sqft must be a number');
-  }
-    }
-
 }
