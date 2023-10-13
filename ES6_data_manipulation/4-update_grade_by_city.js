19c62399343f5c80e0debc704123d55d999d9519
chaher13 <chahercrbs@gmail.com>
2023-10-13 22:57:30 +0200

    indent

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,8 +2,8 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((student) => student.location === city).map((student) => {
     const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id)[0];
     return {
-            ...student,
-            grade: grade ? grade.grade : 'N/A',
+      ...student,
+      grade: grade ? grade.grade : 'N/A',
     };
-    });
+  });
 }
