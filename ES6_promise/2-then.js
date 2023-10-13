18097c0d9e5a96ff84b956eb08c68a1bcfaf3ec8
chaher13 <chahercrbs@gmail.com>
2023-10-13 23:21:31 +0200

    set indent

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 function handleResponseFromAPI(promise) {
   return promise.then(() => ({
-      status: 200,
-      body: 'success',
-    }))
+    status: 200,
+    body: 'success',
+  }))
     .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
 }
